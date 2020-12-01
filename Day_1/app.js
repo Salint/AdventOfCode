40426deb365c516f2036ff49c3b96aa5efb8b4d5
Mohamed Abduljawad <salintofficial@hotmail.com>
2020-12-01 08:25:25 +0200

    Day 1 completion

---
+++ Day_1/app.js
@@ -0,0 +1,41 @@
+const { readFileSync } = require("fs");
+
+const input = readFileSync(__dirname + "/input.txt", "utf8");
+const array = input.split("\n");
+
+
+// First puzzle
+
+console.log("First puzzle");
+
+outer_loop:
+for(let i = 0; i < array.length; i++) {
+
+	for(let j = 0; j < array.length; j++) {
+
+		if((Number(array[i]) + Number(array[j])) == 2020) {
+			console.log(Number(array[i]) * Number(array[j]));
+			break outer_loop;
+		}
+	}
+
+}
+
+console.log("Second puzzle");
+
+outer_loop:
+for(let i = 0; i < array.length; i++) {
+
+	for(let j = 0; j < array.length; j++) {
+
+		for(let k = 0; k < array.length; k++) {
+
+			if((Number(array[i]) + Number(array[j]) + Number(array[k])) == 2020) {
+				console.log(Number(array[i]) * Number(array[j]) * Number(array[k]));
+				break outer_loop;
+			}
+
+		}
+	}
+
+}
